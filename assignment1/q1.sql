eccd0d4796d5cdb4d3babcce45bf513684e049d6
KeshiChen <chenkeshi1@gmail.com>
2018-12-06 19:33:39 +1100

    Add files via upload

---
+++ assignment1/q1.sql
@@ -0,0 +1,27 @@
+﻿select location, time, item, sum(quantity) 
+from test 
+group by location, time, item
+union
+select location, time, '0', sum(quantity)
+from test
+group by location, time
+union
+select location, 0, item, sum(quantity)
+from test
+group by location, item
+union
+select location,0,'0', sum(quantity)
+from test
+group by location
+union
+select '0', time, item, sum(quantity) 
+from test 
+group by  time, item
+union
+select '0', time, '0', sum(quantity) 
+from test 
+group by  time
+union
+select '0', 0, item, sum(quantity) 
+from test 
+group by  item
